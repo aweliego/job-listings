f6922975baaf9334a9e2cf582a7d77a0ee5bdac8
aweliego <amelie.gorry.am@gmail.com>
2023-02-19 10:51:55 +0100

    Config custom styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          primary: 'hsl(180, 29%, 50%)',
+          'bg-color': 'hsl(180, 52%, 96%)',
+          'filter-color': 'hsl(180, 31%, 95%)',
+          dark: 'hsl(180, 8%, 52%)',
+          'very-dark': 'hsl(180, 14%, 20%)',
+        },
+      },
+      fontSize: {
+        base: '0.9375rem',
+      },
+      fontFamily: {
+        body: ['League Spartan'],
+      },
+    },
   },
   plugins: [],
 }
