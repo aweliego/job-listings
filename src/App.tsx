d95585692bed4ce15a45ff7667de8bc55bd3066d
aweliego <amelie.gorry.am@gmail.com>
2023-03-04 14:11:53 +0100

    Render all listings dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Listing from './components/Listing'
 import SearchBar from './components/SearchBar'
+import data from './data.json'
 
 const App = () => {
 
@@ -8,7 +9,23 @@ const App = () => {
       <div className='bg-header-mobile sm:bg-header-desktop bg-no-repeat bg-cyan-primary h-36 w-full'></div>
       <section className='flex flex-col justify-center items-center'>
         <SearchBar />
-        <Listing />
+        {data.map((listing) => {
+          return (<Listing
+            key={listing.id}
+            company={listing.company}
+            logo={listing.logo}
+            newListing={listing.new}
+            featured={listing.featured}
+            position={listing.position}
+            role={listing.role}
+            level={listing.level}
+            postedAt={listing.postedAt}
+            contract={listing.contract}
+            location={listing.location}
+            languages={listing.languages}
+            tools={listing.tools}
+          />)
+        })}
       </section>
     </>
   )
--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,30 +1,60 @@
-import Photosnap from '../assets/images/photosnap.svg'
+// import '../assets/images/photosnap.svg'
 
-const Listing = () => {
+type ListingProps = {
+    key: number
+    company: string
+    logo: string
+    newListing: boolean
+    featured: boolean
+    position: string
+    role: string
+    level: string
+    postedAt: string
+    contract: string
+    location: string
+    languages: string[]
+    tools: string[]
+}
+
+const Listing: React.FC<ListingProps> = ({
+    key,
+    company,
+    logo,
+    newListing,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools
+}) => {
 
     return (
-        <article className='flex flex-wrap sm:flex-row sm:justify-between sm:items-center w-11/12 sm:w-4/5 mx-auto mt-4 p-4 bg-white rounded drop-shadow-lg border-l-4 border-l-cyan-primary cursor-pointer'>
+        <article className='flex flex-wrap sm:flex-row sm:justify-between sm:items-center w-11/12 sm:w-4/5 mx-auto mt-4 mb-9 md:mb-2 p-4 bg-white rounded drop-shadow-lg border-l-4 border-l-cyan-primary cursor-pointer'>
             <div className='md:flex md:flex-row md:items-center relative'>
                 {/* Company image */}
-                <img src={Photosnap} alt='company logo' className='md:mr-6 md:ml-4 w-16 md:w-[5.5rem] absolute -translate-y-12 md:static md:-translate-y-0' />
+                <img src={logo} alt='company logo' className='md:mr-6 md:ml-4 w-16 md:w-[5.5rem] absolute -translate-y-12 md:static md:-translate-y-0' />
 
                 <div className='flex flex-col gap-1'>
                     {/* Company name */}
                     <div className='flex gap-x-3 items-center leading-none mt-7 md:mt-0'>
-                        <p className='text-cyan-primary font-medium'>Photosnap</p>
+                        <p className='text-cyan-primary font-medium'>{company}</p>
                         {/* Badges */}
-                        <span className='badge bg-cyan-primary '>NEW!</span>
-                        <span className='badge bg-cyan-very-dark'>FEATURED</span>
+                        {newListing && <span className='badge bg-cyan-primary'>NEW!</span>}
+                        {featured && <span className='badge bg-cyan-very-dark'>FEATURED</span>}
                     </div>
                     {/* Role */}
                     <h4 className='text-lg font-bold text-cyan-very-dark hover:text-cyan-primary'>
-                        Senior Frontend Developer
+                        {position}
                     </h4>
                     {/* Details */}
                     <div className='flex gap-x-7 text-cyan-dark font-normal'>
-                        <span>1d ago</span>
-                        <span>Full Time</span>
-                        <span>USA only</span>
+                        <span>{postedAt}</span>
+                        <span>{contract}</span>
+                        <span>{location}</span>
                     </div>
                 </div>
             </div>
@@ -34,8 +64,8 @@ const Listing = () => {
 
             {/* Filters */}
             <div className='flex flex-wrap gap-x-1 '>
-                <button className='filter'>Frontend</button>
-                <button className='filter'>Senior</button>
+                <button className='filter'>{role}</button>
+                <button className='filter'>{level}</button>
                 <button className='filter'>HTML</button>
                 <button className='filter'>CSS</button>
                 <button className='filter'>JavaScript</button>
