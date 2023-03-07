4d34a67060d0189b2d7d2c0767bd6729a417b8a6
aweliego <amelie.gorry.am@gmail.com>
2023-03-07 11:54:40 +0100

    Refactor input into textarea and autoresize textarea

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,17 @@
+import React from 'react'
+
 const SearchBar = () => {
+
+    const autoResize = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const textarea = e.target as HTMLTextAreaElement
+        textarea.style.height = '56px'
+        const scrollHeight = textarea.scrollHeight
+        textarea.style.height = scrollHeight + 'px'
+    }
 
     return (
         <form className='-translate-y-7 relative w-11/12 sm:w-4/5 mx-auto block'>
-            <input type='text' className='w-full h-14 rounded drop-shadow-lg outline-none px-4' />
+            <textarea onChange={(e) => autoResize(e)} rows={1} className='w-full rounded drop-shadow-lg outline-none p-4 resize-none overflow-hidden'></textarea>
             <button className='absolute top-5 right-5 text-cyan-dark hover:text-cyan-primary hover:underline'>Clear</button>
         </form >
     )
