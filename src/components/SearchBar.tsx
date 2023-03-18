47f19c5ed89acc91ae0d0afbfca6241a9f878ad2
aweliego <amelie.gorry.am@gmail.com>
2023-03-18 19:50:09 +0100

    Set up input handling, submitting and clearing

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const SearchBar = () => {
+    const [value, setValue] = React.useState<string>()
 
     const autoResize = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         const textarea = e.target as HTMLTextAreaElement
@@ -9,10 +10,20 @@ const SearchBar = () => {
         textarea.style.height = scrollHeight + 'px'
     }
 
+    const handleChange = (e: any) => {
+        setValue(e.currentTarget.value)
+    }
+
+    const handleSubmit = (e: any) => {
+        if (e.keyCode == 13 && e.shiftKey == false) {
+            e.preventDefault()
+        }
+    }
+
     return (
         <form className='-translate-y-7 relative w-11/12 sm:w-4/5 mx-auto block'>
-            <textarea onChange={(e) => autoResize(e)} rows={1} className='w-full rounded drop-shadow-lg outline-none p-4 resize-none overflow-hidden'></textarea>
-            <button className='absolute top-5 right-5 text-cyan-dark hover:text-cyan-primary hover:underline'>Clear</button>
+            <textarea value={value} onKeyDown={(e) => handleSubmit(e)} onChange={(e) => { handleChange(e); autoResize(e) }} rows={1} className='w-full rounded drop-shadow-lg outline-none p-4 resize-none overflow-hidden'></textarea>
+            <button type='button' onClick={() => setValue('')} className='absolute top-5 right-5 text-cyan-dark hover:text-cyan-primary hover:underline'>Clear</button>
         </form >
     )
 }
