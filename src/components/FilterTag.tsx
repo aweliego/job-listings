d53bcfc69615194d78563a7f2e3a1bf2d1335b53
aweliego <amelie.gorry.am@gmail.com>
2023-06-17 12:05:34 +0200

    Add remove filter functionality

--- src/components/FilterTag.tsx
+++ src/components/FilterTag.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import RemoveIcon from '../../public/assets/images/icon-remove.svg'
 
 type FilterTagProps = {
+    id: number
     title: string
+    removeFilter: (id: number) => void
 }
 
-const FilterTag: React.FC<FilterTagProps> = ({ title }) => {
+const FilterTag: React.FC<FilterTagProps> = ({ title, id, removeFilter }) => {
 
     return (
         <div className='flex flex-nowrap'>
@@ -13,6 +15,7 @@ const FilterTag: React.FC<FilterTagProps> = ({ title }) => {
                 {title}
             </span>
             <img src={RemoveIcon} alt="remove filter"
+                onClick={() => removeFilter(id)}
                 className='inline-block font-semibold text-white bg-cyan-primary hover:bg-cyan-very-dark rounded-r-sm ml-0 mr-3 px-2 py-2 h-7 cursor-pointer' />
         </div>
     )
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -9,11 +9,12 @@ type SearchBarProps = {
 
 const SearchBar: React.FC<SearchBarProps> = ({ filters, setFilters }) => {
 
+    const handleRemoveFilter = (id: number) => setFilters(prevFilters => prevFilters.filter(prevFilter => prevFilter.id !== id))
+
     return (
         <div className='grid grid-cols-8 -translate-y-7 w-11/12 sm:w-4/5 mx-auto drop-shadow-lg'>
             <div className='col-span-6 md:col-span-7 xs:col-span-6 flex flex-wrap gap-y-3 rounded-l-sm p-4 md:pl-8 bg-white'>
-                {filters.map(filter => <FilterTag id={filter.id} title={filter.title} setFilters={setFilters} />)}
-
+                {filters.map(filter => <FilterTag id={filter.id} title={filter.title} removeFilter={handleRemoveFilter} />)}
             </div>
             <button type='button' onClick={() => setFilters([])}
                 className='col-span-2 md:col-span-1 text-cyan-dark hover:text-cyan-primary hover:underline font-semibold rounded-r-sm bg-white '>Clear</button>
